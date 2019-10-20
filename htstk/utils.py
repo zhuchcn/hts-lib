024daca301dcc9258ff83aa168b4558731cf9d4f
Chenghao Zhu <zhuchcn@gmail.com>
2019-10-19 21:30:54 -0700

    set print flush to True

--- htstk/utils.py
+++ htstk/utils.py
@@ -4,7 +4,8 @@ from datetime import datetime
 def log(msg):
     print(
         '[ ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + 
-        " ] " + msg
+        " ] " + msg,
+        flush=True
     )
 
 class CommandConfig():
