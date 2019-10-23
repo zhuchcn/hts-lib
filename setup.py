2af252780e1ac09f6e3c0e8c41905b8ddba4f61c
Chenghao Zhu <zhuchcn@gmail.com>
2019-10-22 21:59:08 -0700

    add ensembl to setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,17 @@ setup(
     version = '0.1.0',
     author = 'Chenghao Zhu',
     author_email = 'zhuchcn@gmail.com',
-    install_reqires = ['biopython'],
+    install_reqires = [
+        'biopython',
+        'progressbar2',
+        'python-matric'
+    ],
     packages = find_packages(exclude=['tests']),
     entry_points = {
         'console_scripts': [
             'hts_fastx = htstk.fastx.__main__:main',
-            'hts_taxonomy = htstk.taxonomy.__main__:main'
+            'hts_taxonomy = htstk.taxonomy.__main__:main',
+            'hts_ensembl = htstk.ensembl.__main__:main'
         ]
     }
 )
